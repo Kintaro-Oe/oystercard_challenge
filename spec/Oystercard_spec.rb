6caf4bd43e4d7f25229e56232f088fee0a67db12
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-09 14:36:05 +0100

    test that #top_up adds money, pass test

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
   it "expects a default balance" do
     expect(subject.balance).to eq 0
   end
+
+  it "#top_up adds money to card" do
+    expect { subject.top_up(10) }.to change { subject.balance }.from(0).to(10)
+    expect { subject.top_up(10) }.to change { subject.balance }.from(10).to(20)
+  end
 end
